78755a86c18460d6406c2c0e72312fa9e1f6791b
likun <1617455243@qq.com>
2018-08-07 22:30:40 +0800

    '首页-个人信息'

---
+++ docs/.vuepress/components/Right.vue
@@ -0,0 +1,19 @@
+<!--  -->
+<template>
+  <div class="right">right</div>
+</template>
+
+<script type='text/ecmascript-6'>
+export default {
+}
+</script>
+<style scoped lang='stylus' rel='stylesheet/stylus'>
+  .right
+    position: absolute
+    right: 0
+    height: 100%
+    width: 20vw
+    min-width: 200px
+    background: #f5f5f5
+    box-shadow: -5px 5px 5px #888888;
+</style>
